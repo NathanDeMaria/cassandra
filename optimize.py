c21c0e223d216974945a350bab0d9a697e3f920e
NathanDeMaria <nathandmaria@gmail.com>
2026-01-25 19:59:54 +0000

    Optimization script

---
+++ optimize.py
@@ -0,0 +1,58 @@
+import asyncio
+from dataclasses import asdict
+from functools import partial
+
+import pandas as pd
+from bayes_opt import BayesianOptimization
+
+from prediction_evaluation.predictor import Elo538Predictor, Predictor
+from prediction_evaluation.save_predictions import (
+    Config,
+    NcaabbGender,
+    OddsDatabase,
+    Season,
+    join_with_odds,
+    read_all_seasons,
+)
+
+
+def _negative_brier_score(
+    seasons: list[Season],
+    odds_db: OddsDatabase,
+    predictor_class: type[Predictor],
+    **kwargs,
+) -> float:
+    predictor = predictor_class(**kwargs)
+    prediction_results = join_with_odds(predictor, seasons, odds_db)
+    df = pd.DataFrame([asdict(result) for result in prediction_results])
+    return -((df["team1_win_prob"] - df["team1_win"]) ** 2).mean()
+
+
+async def _main() -> None:
+    # TODO: make this configurable
+    pbounds = {"home_advantage": (0, 300), "k": (1, 40)}
+    predictor_class = Elo538Predictor
+    gender = NcaabbGender.womens
+
+    config = Config.init_from_file()
+    seasons = [s async for s in read_all_seasons(gender, config.bucket)]
+    odds_db = await OddsDatabase.from_s3(config.bucket)
+
+    optimizer = BayesianOptimization(
+        f=partial(
+            _negative_brier_score,
+            seasons=seasons,
+            odds_db=odds_db,
+            predictor_class=predictor_class,
+        ),
+        pbounds=pbounds,
+        random_state=1,
+    )
+
+    optimizer.maximize(n_iter=100)
+
+    print(optimizer.max)
+
+
+if __name__ == "__main__":
+    asyncio.run(_main())
