4cf33a1b24a27012bc0867c6708b8c10ce3d742a
NathanDeMaria <nathandmaria@gmail.com>
2026-08-02 02:37:10 +0000

    Use ty, CI :)

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
 lint:
 	poetry run ruff check --fix .
-	poetry run mypy .
+	poetry run ty check .
+
+
+# Same checks as `lint`, but reports instead of fixing (what CI runs)
+check:
+	poetry run ruff check .
+	poetry run ty check .
 
 
 test:
 	poetry run pytest .
+
+
+.PHONY: lint check test
--- cassandra/optimize.py
+++ cassandra/optimize.py
@@ -14,7 +14,7 @@ def optimize(
         f=function,
         # The docs confirm list-of-str is how you do categorical
         # https://bayesian-optimization.github.io/BayesianOptimization/3.2.0/parameter_types.html#3.-Categorical-variables
-        pbounds=param_bounds,  # type: ignore[arg-type]
+        pbounds=param_bounds,  # ty: ignore[invalid-argument-type]
         random_state=1,
     )
 
